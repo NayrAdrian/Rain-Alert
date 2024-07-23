aca72652274644e67028275a2518a1d7a3ccb298
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-23 16:42:54 +0800

    Initial Commit

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "9d8bc79a5475666165e79e492993d334"  # Sample API Key from Sample Account
 
-MY_LAT = 14.612034
-MY_LONG = 121.086786
+MY_LAT = 14.612034  # Sample Coordinates
+MY_LONG = 121.086786  # Sample Coordinates
 
 weather_params = {
     "lat": MY_LAT,
